5266b0b96a016b5e8ac40703ad0f46041d509597
indraagstin025 <indraagustin012@gmail.com>
2025-04-26 22:58:21 +0700

    Pertemuan 3

---
+++ Pertemuan 3/my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./components/pages/Dashboard";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
